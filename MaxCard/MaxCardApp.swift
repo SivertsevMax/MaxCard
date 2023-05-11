6fa0b03c9ae4ecfaeb53e86097fd6779a4e37c63
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-05-11 18:48:28 +0300

    Initial Commit

---
+++ MaxCard/MaxCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxCardApp.swift
+//  MaxCard
+//
+//  Created by Максим Сиверцев on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MaxCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
